4f27a5e4df52dd8ec185524a59ccb2f0c38e6956
Rayan <rayansingh2002@gmail.com>
2025-07-28 00:02:14 +0100

    Added basic server and client

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = '127.0.0.1' # My local machine
+PORT = 65432 # Port to listen on
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((HOST, PORT))
+server.listen()
+print(f"Server listening on {HOST}:{PORT}")
+
+
